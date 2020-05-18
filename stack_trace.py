6a0eed230bc53ae458291419352dde257300ffaa
swapnilravi10 <swapnil.ravi10@gmail.com>
2020-05-18 22:48:14 +0530

    add each paragraph in list

--- stack_trace.py
+++ stack_trace.py
@@ -3,22 +3,19 @@ file_location =  r"C:\Users\Swapnil\projects\Python_demo\app_log.txt"
 
 app_log_files = file_location
 
-##Open file
-with open(app_log_files) as f:
-	##readlines
-	f = f.readlines()
+f = open(app_log_files, 'r')
 
-	##Find by year
-	date = '2020'
+data = f.read()
 
-	count = 1
+splat = data.split("\n\n")
+for numbers, paragraph in enumerate(splat, 1):
 	list_of_stack = []
-	for logs in f:
-		if date in logs:
-			count += 1
-			list_of_stack.append(logs)
-	##Give total lines with 2020		
-	print('Count',count)
-	##Each line is stored in a list seperated by comma.
-	sorted_list = [x for xs in list_of_stack for x in xs.split(",")]
-	print(sorted_list)
+	list_of_stack.append(paragraph)
+	print(list_of_stack)
+	# list_list_stack = [list_of_stack]
+	# # print(list_list_stack)
+			
+	
+			
+
+			
